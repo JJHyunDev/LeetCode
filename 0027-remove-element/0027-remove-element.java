9091c02c18b254ad4721ae34f8f46bcd58d29f7f
JJHyunDev <bellhyunforgithub@gmail.com>
2025-06-25 00:53:03 +0900

    Time: 0 ms (100%), Space: 42.3 MB (8.52%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -4,7 +4,7 @@ class Solution {
         for(int right = 0; right < nums.length; right++){
             if(nums[right] != val){
                 nums[left] = nums[right];
-                nums[right] = nums[left];
+                
                 left++;
             }
         }
