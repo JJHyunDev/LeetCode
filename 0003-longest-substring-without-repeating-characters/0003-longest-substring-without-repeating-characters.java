4e704d5849c88336edd1c78eba271a440f57d702
JJHyunDev <bellhyunforgithub@gmail.com>
2025-07-17 23:46:39 +0900

    Time: 5 ms (88.91%), Space: 44.7 MB (57.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,12 +7,12 @@ class Solution {
         for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
 
-            if(map.containsKey(c) && map.get(c) >= left){// 기존에 c가 있다면 
-                left = map.get(c) + 1;
+            if(map.containsKey(c) && map.get(c) >= left){// 기존에 c가 있다면 해당 value에서 + 1 해준다.
+                left = map.get(c) + 1; // 예) abc 에서 left는 a를 가리키다가 abca가 되면 value + 1로 b를 가리킨다.
             }
 
             map.put(c, right); //c의 value 최신화
-            max = Math.max(max, right - left + 1); // 최대 길이 갱신
+            max = Math.max(max, right - left + 1); // 매 반복마다 최대 길이 갱신
         }
     
         return max;
