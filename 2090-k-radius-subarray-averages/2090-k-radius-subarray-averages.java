6bba45941c7b813e8410a5485a04d4d56ba82ccb
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-03 09:52:32 +0900

    Time: 7 ms (26.26%), Space: 64.3 MB (43.14%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,27 +1,28 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int left = 0;
         int n = nums.length;
-        int size = (2 * k) + 1;
-        int[] avg = new int[n];
-        Arrays.fill(avg, -1);
-        if(n < size){
-            return avg;
+        int size = 2 * k + 1;
+        int ans[] = new int[n];
+        Arrays.fill(ans, -1);
+        if(n < size) {
+            return ans;
         }
 
         long sum = 0;
-        for(int right = 0; right < n; right++){
+        int left = 0;
+        for(int right = 0; right < n; right++) {
             sum += nums[right];
 
-            if(right - left + 1 > size ){
+            if(right - left + 1 > size) {
                 sum -= nums[left];
                 left++;
             }
 
-            if(right - left + 1 == size){
-                avg[k++] = (int)(sum / (right - left + 1));
+            if(right - left + 1 == size) {
+                ans[k++] = (int) (sum / (right - left + 1));
             }
         }
-        return avg;
+        
+        return ans;
     }
 }
