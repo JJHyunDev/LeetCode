b11652159edceb1499499c8e057de10aedc4cb70
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-03 10:41:37 +0900

    Time: 2 ms (10.6%), Space: 41.7 MB (6.49%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -2,17 +2,19 @@ class Solution {
     public int minStartValue(int[] nums) {
         int startValue = 1;
 
-        while(true){
+        while(true) {
             int temp = startValue;
-            for(int i = 0; i < nums.length; i++){
+            for(int i = 0; i < nums.length; i++) {
                 temp += nums[i];
-                if(temp < 1){
+
+                if(temp < 1) {
                     break;
                 }
-                if(i == nums.length-1){
+                if(i == nums.length - 1) {
                     return startValue;
                 }
             }
+
             startValue++;
         }
     }
