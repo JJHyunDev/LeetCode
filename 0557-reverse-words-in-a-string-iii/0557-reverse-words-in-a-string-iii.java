75c8a7c2c10b6f30484fe7b2fe581e6d65d44ee7
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-04 09:24:18 +0900

    Time: 16 ms (17.67%), Space: 45.5 MB (32.75%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,21 +1,20 @@
 class Solution {
     public String reverseWords(String s) {
+        // split(" ");
+        // 문자열 배열을 만들고
+        // 각 배열 요소를 reverse한 후 StringBuilder.append 한다.
+        String arr[] = s.split(" ");
         String answer = "";
-        String[] str = s.split(" ");
-        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < str.length; i++){
-            if(i>0){
-                sb.append(" ");
-            }
-            int right = str[i].length()-1;
-            while(right>=0){
-                sb.append(str[i].charAt(right));
-                right--;
-            }
-            answer += sb.toString();
-            sb.setLength(0);
+        for(String str : arr) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(str);
+            sb.reverse();
+
+            answer += sb + " ";            
         }
-        return answer;
+
+        return answer.trim();
     }
 }
