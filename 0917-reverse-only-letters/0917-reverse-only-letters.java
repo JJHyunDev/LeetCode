56eebb8687cde8dc2e04cfa133ae75b0418f413f
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-04 09:48:58 +0900

    Time: 1 ms (25.61%), Space: 41.9 MB (16.4%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.java
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -1,29 +1,19 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
-        char[] arr = s.toCharArray();
-        int left = 0;
-        int right = arr.length-1;
-        while(left < right){
-            // arr[left] 가 a-z or A-Z가 아니라면
-            if(!((arr[left] >= 'a' && arr[left] <= 'z') || (arr[left] >= 'A' && arr[left] <= 'Z'))){
-                left++;
+        StringBuilder sb = new StringBuilder();
+        int right = s.length() - 1;
+
+        for(int left = 0; left < s.length(); left++) {
+            if(Character.isLetter(s.charAt(left))) {
+                while(!Character.isLetter(s.charAt(right))){
+                    right--;
+                }
+                sb.append(s.charAt(right--));
+            } else {
+                sb.append(s.charAt(left));
             }
-            // arr[right] 가 a-z or A-Z가 아니라면
-            else if(!((arr[right] >= 'a' && arr[right] <= 'z') || (arr[right] >= 'A' && arr[right] <= 'Z'))){
-                right--;
-            }
-            else{
-                char temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;
-                right--;
-            } 
         }
-        String answer = "";
-        for(char c : arr){
-            answer += c;
-        }
-        return answer;
+
+        return sb.toString();
     }
 }
