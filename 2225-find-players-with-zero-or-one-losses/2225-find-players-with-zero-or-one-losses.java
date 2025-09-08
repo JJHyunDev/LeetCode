fb3c1d352b268253cde50315eafd2d1228fb0116
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-08 11:40:43 +0900

    Time: 82 ms (56.34%), Space: 111.2 MB (16.1%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        HashMap<Integer, Integer> winneri = new HashMap<>(); 
+        HashMap<Integer, Integer> winneri = new HashMap<>();
         HashMap<Integer, Integer> loseri = new HashMap<>();
 
-        for(int i = 0; i < matches.length; i++){
+        for(int i= 0; i < matches.length; i++) {
             int winner = matches[i][0];
             int loser = matches[i][1];
+
             winneri.put(winner, winneri.getOrDefault(winner, 0) + 1);
             loseri.put(loser, loseri.getOrDefault(loser, 0) + 1);
         }
 
         List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> winTemp = new ArrayList<>(); // List를 따로 선언해서 사용해줬다.
-        for(int i : winneri.keySet()){
-            if(!loseri.containsKey(i)){
+        List<Integer> winTemp = new ArrayList<>();
+        for(int i : winneri.keySet()) {
+            if(!loseri.containsKey(i)) {
                 winTemp.add(i);
             }
-        }
+        }  
         Collections.sort(winTemp);
         answer.add(winTemp);
-        
-        List<Integer> loseTemp = new ArrayList<>(); // List를 따로 선언함.
-        for(int i : loseri.keySet()){
-            if(loseri.get(i) == 1){
+
+        List<Integer> loseTemp = new ArrayList<>();
+        for(int i : loseri.keySet()) {
+            if(loseri.get(i) == 1) {
                 loseTemp.add(i);
             }
         }
