ccf0678f0f9eee21efc7e7a5899eeaa791bef2d2
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-10 10:59:31 +0900

    Time: 86 ms (13.55%), Space: 55.3 MB (7.03%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,32 +1,26 @@
-import java.util.HashMap;
-
 class Solution {
     public int equalPairs(int[][] grid) {
         HashMap<String, Integer> map = new HashMap<>();
-        int n = grid.length;
-        
-        // 모든 행을 문자열로 변환해 카운트
-        for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                sb.append(grid[i][j]).append(",");
+
+        for(int i = 0; i < grid.length; i++) {
+            String col = "";
+            for(int j = 0; j < grid[i].length; j++) {
+                col += grid[i][j] + ",";
             }
-            String key = sb.toString();
-            map.put(key, map.getOrDefault(key, 0) + 1);
+
+            map.put(col, map.getOrDefault(col, 0) + 1);
         }
-        
+
         int ans = 0;
-        
-        //  모든 열을 문자열로 변환 후 행 맵과 비교
-        for (int j = 0; j < n; j++) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n; i++) {
-                sb.append(grid[i][j]).append(",");
+        for(int i = 0; i < grid.length; i++) {
+            String col = "";
+            for(int j = 0; j < grid[i].length; j++) {
+                col += grid[j][i] + ",";
             }
-            String key = sb.toString();
-            ans += map.getOrDefault(key, 0); // 일치하는 행의 개수 누적
+
+            ans += map.getOrDefault(col, 0);            
         }
-        
+
         return ans;
     }
 }
