16e856cccb61947efa0ba9561c89a28063b1e9eb
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-20 09:38:20 +0900

    Time: 32 ms (42.52%), Space: 50.5 MB (85.59%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) ->
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->
             (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
         );
 
-        for(int[] i : points){
-            heap.add(i);
-            if(heap.size() > k){
-                heap.remove();
+        for(int[] i : points) {
+            pq.add(i);
+            if(pq.size() > k) {
+                pq.remove();
             }
         }
 
         int[][] ans = new int[k][2];
         int idx = 0;
-        while(!heap.isEmpty()){
-            ans[idx++] = heap.remove();
+        while(!pq.isEmpty()) {
+            ans[idx++] = pq.remove();
         }
 
         return ans;
