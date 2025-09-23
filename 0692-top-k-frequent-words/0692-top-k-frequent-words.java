5e1668efa0f2f7f2fef4aa241e5884ad2859b3a8
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-23 23:15:12 +0900

    Time: 6 ms (98.05%), Space: 44.5 MB (95.52%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,30 +1,27 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         HashMap<String, Integer> map = new HashMap<>();
-
-        for(String s : words){ // words 돌면서 map 초기화
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        
+        for(String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        PriorityQueue<HashMap.Entry<String, Integer>> heap = new PriorityQueue<>((a, b) ->
-            {
-                if(b.getValue() -a.getValue() != 0){
-                    return b.getValue() -a.getValue();
-                }
-                return a.getKey().compareTo(b.getKey());
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a, b) -> {
+            if(b.getValue() - a.getValue() != 0) {
+                return b.getValue() - a.getValue();
+            }
+            return a.getKey().compareTo(b.getKey());
         });
 
-        for(HashMap.Entry<String, Integer> entry : map.entrySet()){
-            heap.add(entry);
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            pq.add(entry);
         }
 
-        List<String> ans = new ArrayList<>();
-        while(k > 0){
-            String s = heap.remove().getKey();
-            ans.add(s);
-            k--;
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < k; i++) {
+            answer.add(pq.remove().getKey());
         }
-    
-        return ans;
+
+        return answer;
     }
 }
