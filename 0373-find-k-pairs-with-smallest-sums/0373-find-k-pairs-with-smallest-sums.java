e19d502fcd1b7dc26753e02d62f013c855261a1a
JJHyunDev <bellhyunforgithub@gmail.com>
2025-09-29 09:12:39 +0900

    Time: 35 ms (50.39%), Space: 63 MB (27.37%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,60 +1,28 @@
-// class Solution {
-//     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-//         PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> 
-//         {
-//             return (a[0] + a[1]) - (b[0] + b[1]);
-//         });
-
-//         List<List<Integer>> answer = new ArrayList<>();
-
-//         for(int i : nums2){
-//             heap.offer(new int[] {nums1[0], i});
-//         }
-
-//         while(k > 0 && !heap.isEmpty()){
-//             k--;
-//             List<Integer> list = new ArrayList<>();
-//             int[] arr = heap.poll();
-//             list.add(arr[0]);
-//             list.add(arr[1]);
-
-//             if(idx + 1 < nums1.length){
-//                 idx++;
-//                 heap.offer(new int[] {nums1[idx], arr[1]});
-//             }
-
-//             answer.add(list);
-//         }
-        
-//         return answer;
-//     }
-// }
-
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
-            return result;
+        List<List<Integer>> answer = new ArrayList<>();
+        if(nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return answer;
         }
-        
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> 
-            (nums1[a[0]] + nums2[a[1]]) - (nums1[b[0]] + nums2[b[1]]));
-        
-        for (int j = 0; j < nums2.length; j++) {
-            heap.offer(new int[]{0, j});
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
+        (nums1[a[0]] + nums2[a[1]]) - (nums1[b[0]] + nums2[b[1]]));
+
+        for(int j = 0; j < nums2.length; j++) {
+            pq.offer(new int[]{0, j});
         }
-        
-        while (k-- > 0 && !heap.isEmpty()) {
-            int[] indices = heap.poll();
-            int i = indices[0];
-            int j = indices[1];
-            result.add(Arrays.asList(nums1[i], nums2[j]));
-            
-            if (i + 1 < nums1.length) {
-                heap.offer(new int[]{i + 1, j});
+
+        while(k-- > 0 && !pq.isEmpty()) {
+            int[] arr = pq.poll();
+            int i = arr[0];
+            int j = arr[1];
+            answer.add(Arrays.asList(nums1[i], nums2[j]));
+
+            if(i + 1 < nums1.length) {
+                pq.offer(new int[]{i + 1, j});
             }
         }
-        
-        return result;
+
+        return answer;
     }
 }
