f19b7b87007d9c43c0cea0039e1978dd68f22cc0
정 종 현 <bellhyunforgithub@gmail.com>
2025-10-30 11:22:17 +0900

    Time: 51 ms (35.6%), Space: 43.9 MB (95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,19 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-
     public int lengthOfLIS(int[] nums) {
-        int ans = 0;
-        for(int i = 0; i < nums.length; i++) {
-            ans = Math.max(ans, dp(i, nums));
-        }
-
-        return ans;
-    }
-
-    public int dp(int i, int[] nums) {
-        if(memo.containsKey(i)) {
-            return memo.get(i);
-        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
 
         int ans = 1;
-
-        for(int j = 0; j < i; j++) {
-            if(nums[i] > nums[j]) {
-                ans = Math.max(ans, dp(j, nums) + 1);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    ans = Math.max(ans, dp[i]);
+                }
             }
         }
 
-        memo.put(i, ans);
         return ans;
     }
 }
