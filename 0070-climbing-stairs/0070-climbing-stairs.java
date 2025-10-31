04ea36eda2a2c22b02547a00d7ddee2bdcd8a7d2
정 종 현 <bellhyunforgithub@gmail.com>
2025-10-31 09:42:29 +0900

    Time: 0 ms (100%), Space: 40.6 MB (40.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,5 +1,11 @@
 class Solution {
+    int[] memo;
     public int climbStairs(int n) {
+        memo = new int[n + 1];
+        return solve(n);
+    }
+
+    public int solve(int n) {
         if(n == 1) {
             return 1;
         }
@@ -8,15 +14,16 @@ class Solution {
             return 2;
         }
 
-        int[] dp = new int[n + 1];
-
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        if(memo[n] != 0) {
+            return memo[n];
         }
 
-        return dp[n];
+        memo[1] = 1;
+        memo[2] = 2;
+
+        int way = solve(n - 1) + solve(n - 2);
+
+        memo[n] = way;
+        return way;
     }
 }
