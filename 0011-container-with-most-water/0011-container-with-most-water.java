ca1afd433713c19ce682ada72d40a0a55e33b27b
정 종 현 <bellhyunforgithub@gmail.com>
2025-12-16 23:42:07 +0900

    Time: 5 ms (77.23%), Space: 77.4 MB (29.88%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,15 +4,15 @@ class Solution {
         int left = 0;
         int right = height.length - 1;
 
-        while(left < right){
+        while(left < right) {
             int len = right - left;
             int min = Math.min(height[left], height[right]);
             int area = len * min;
             max = Math.max(max, area);
-            if(height[left] < height[right]){
-                left++;
-            }else {
+            if(height[left] > height[right]) {
                 right--;
+            } else {
+                left++;
             }
         }
 
