1e952612a50aa49fad058c22182fb315f1a0d10e
정 종 현 <bellhyunforgithub@gmail.com>
2025-12-23 21:02:11 +0900

    Time: 89 ms (37.32%), Space: 95.5 MB (73.35%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        if (n == 0) return 0;
+
+        double[][] cars = new double[n][2];
+        for(int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double) (target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double currentFleetTime = 0;
+
+        for(int i = 0; i < n; i++) {
+            double arrivalTime = cars[i][1];
+
+            if(arrivalTime > currentFleetTime) {
+                fleets++;
+                currentFleetTime = arrivalTime;
+            }
+        }
+
+        return fleets;
+    }
+}
