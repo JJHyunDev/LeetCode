64fe698d2f829fb102198dc5011791f5e9e64ce5
정 종 현 <bellhyunforgithub@gmail.com>
2026-01-06 09:16:03 +0900

    Time: 34 ms (22.7%), Space: 46.9 MB (16.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character, Integer> s1Map = new HashMap<>();
+        Map<Character, Integer> s1Map = new HashMap<>();
+        Map<Character, Integer> s2Map = new HashMap<>();
 
-        for(char c : s1.toCharArray()) {
+        if(s1.length() > s2.length()) {
+            return false;
+        }
+
+        for(int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
             s1Map.put(c, s1Map.getOrDefault(c, 0) + 1);
         }
 
-        HashMap<Character, Integer> s2Map = new HashMap<>();
         int left = 0;
-        for(int right = 0; right < s2.length(); right++) {
-            char rightChar = s2.charAt(right);
-            s2Map.put(rightChar, s2Map.getOrDefault(rightChar, 0) + 1);
+        for(int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
+            s2Map.put(c, s2Map.getOrDefault(c, 0) + 1);
 
-            if(right - left + 1 == s1.length()) {
+            if(i - left + 1 == s1.length()) {
                 if(s1Map.equals(s2Map)) {
-                    return true;
+                return true;
                 }
 
-                char leftChar = s2.charAt(left);
-                s2Map.put(leftChar, s2Map.get(leftChar) - 1);
-                if(s2Map.get(leftChar) == 0) {
-                    s2Map.remove(leftChar);
+                
+                char ch = s2.charAt(left);
+                s2Map.put(ch, s2Map.get(ch) - 1);
+                if(s2Map.get(ch) == 0) {
+                    s2Map.remove(ch);
                 }
-
                 left++;
+                
             }
         }
 
-        return false;
+        return s1Map.equals(s2Map);
     }
 }
