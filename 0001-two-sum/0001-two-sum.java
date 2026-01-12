a892a238b9873c6a5766a0bfcbfe680ec70e5198
정 종 현 <bellhyunforgithub@gmail.com>
2026-01-12 23:03:11 +0900

    Time: 2 ms (99.13%), Space: 45 MB (99.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int answer[] = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
-                }
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+            if(map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
             }
-        }
 
-        return answer;        
+            map.put(num, i);
+        }
+        
+        return new int[]{-1, -1}; 
     }
 }
