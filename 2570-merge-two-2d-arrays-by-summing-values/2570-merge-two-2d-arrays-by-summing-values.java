11358cc3ecec7dca6488a2fa3a9670687023e871
정 종 현 <bellhyunforgithub@gmail.com>
2026-04-30 00:30:49 +0900

    Time: 1 ms (88.02%), Space: 46.7 MB (56.53%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,34 +1,35 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
         List<int[]> ans = new ArrayList<>();
-        int p1 = 0, p2 = 0;
-        int n1 = nums1.length, n2 = nums2.length;
+        int i = 0, j = 0;
 
-        while(p1 < n1 && p2 < n2) {
-            int id1 = nums1[p1][0], val1 = nums1[p1][1];
-            int id2 = nums2[p2][0], val2 = nums2[p2][1];
+        while(i < nums1.length && j < nums2.length) {
+            int id1 = nums1[i][0];
+            int val1 = nums1[i][1];
+            int id2 = nums2[j][0];
+            int val2 = nums2[j][1];
 
             if(id1 == id2) {
                 ans.add(new int[]{id1, val1 + val2});
-                p1++;
-                p2++;
+                i++;
+                j++;
             } else if(id1 < id2) {
                 ans.add(new int[]{id1, val1});
-                p1++;
+                i++;
             } else {
                 ans.add(new int[]{id2, val2});
-                p2++;
+                j++;
             }
         }
-
-        while(p1 < n1) {
-            ans.add(nums1[p1++]);
-        }
-
-        while(p2 < n2) {
-            ans.add(nums2[p2++]);
-        }
         
+        while(i < nums1.length) {
+            ans.add(nums1[i++]);
+        }
+
+        while(j < nums2.length) {
+            ans.add(nums2[j++]);
+        }
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
