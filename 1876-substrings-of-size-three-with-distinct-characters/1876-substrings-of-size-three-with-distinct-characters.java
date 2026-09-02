ecda8749a348e6983128aade8520d15cbcf8506b
정 종 현 <bellhyunforgithub@gmail.com>
2026-09-02 21:08:15 +0900

    Time: 1 ms (96.45%), Space: 43 MB (53.43%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -3,17 +3,15 @@ class Solution {
         int cnt = 0;
         int n = s.length();
 
-        if(n < 3) return 0;
-
-        for(int i = 0; i <= n - 3; i++) {
+        for (int i = 0; i <= n - 3; i++) {
             char a = s.charAt(i);
             char b = s.charAt(i + 1);
             char c = s.charAt(i + 2);
 
-            if(a != b && b != c && a != c) {
+            if(a != b && b != c && c != a) {
                 cnt++;
             }
-        }
+        } 
 
         return cnt;
     }
